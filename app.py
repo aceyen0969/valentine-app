8e9dbd106f23b84b19be736fcde65b9a0be85fef
Aceyen <pinedakeat@gmail.com>
2026-02-14 12:30:33 -0800

    Fixed Render production app

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
+# ----- FLASK APP SETUP -----
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'valentine_secret'
+
+# Use absolute path for SQLite for Render
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "site.db")}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
 db = SQLAlchemy(app)
 
+# ----- DATABASE MODEL -----
 class Confession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 
+# ----- ROUTES -----
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -21,7 +26,7 @@ def home():
 @app.route('/confessions', methods=['GET', 'POST'])
 def confessions():
     if request.method == 'POST':
-        message = request.form['message']
+        message = request.form.get('message')
         if message:
             new_confession = Confession(message=message)
             db.session.add(new_confession)
@@ -31,7 +36,10 @@ def confessions():
     all_confessions = Confession.query.order_by(Confession.date_posted.desc()).all()
     return render_template("confessions.html", confessions=all_confessions)
 
+# ----- ENSURE DATABASE EXISTS -----
+with app.app_context():
+    db.create_all()
+
+# ----- LOCAL DEVELOPMENT -----
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
